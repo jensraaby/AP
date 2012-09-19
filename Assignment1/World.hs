374982ae1cccbb56d77502db845442f0fcb5acfd
Jens Raaby <jens+github@m.raaby.co.uk>
2012-09-19 19:58:20 +0200

    added a bit more to the assignment

--- Assignment1/World.hs
+++ Assignment1/World.hs
@@ -1,13 +1,37 @@
-module World
+{- 
+put this back after World: 
 ( validMove
 , fromList
-) where
+) 
+-} 
+module World (fromList,Position,Direction)
+where
 
 data Direction = North | South | West | East 
                deriving (Show,Eq,Read,Enum)
 type Position = (Int, Int)
 
+-- type Cell - a list of walls for that cell (Eg east and north)
+type Cell = [Direction]
+
+-- functions for manipulating positions
+move :: Direction -> Position -> Position
+move North (x,y) = (x, y+1)
+move West  (x,y) = (x-1, y)
+move East  (x,y) = (x+1, y)
+move South (x,y) = (x, y-1)
 
 
-validMove :: Maze -> Position -> Position -> Bool
+-- a maze is a grid of cells (M rows, N columns). cells denoted by pairs of integers, origin lower left
+type Maze = [[Cell]]
+
+sizeMaze :: Maze -> (Int,Int)
+sizeMaze = undefined
+-- functions for querying the maze in various ways
+
+--validMove :: Maze -> Position -> Position -> Bool
+--validMove = undefined
+
 fromList :: [(Position, [Direction])] -> Maze
+fromList = undefined
+
