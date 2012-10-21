cceb1d0b5202acc1bc15e9f87a2facb9a48d3715
Jens Raaby <jens+github@m.raaby.co.uk>
2012-10-21 21:26:22 +0100

    Working MapReduce framework

--- Assignment3/code/mr.erl
+++ Assignment3/code/mr.erl
@@ -1,32 +1,57 @@
--module(mr).
+%%%% MapReduce Framework in Erlang
+%%%% October 2012
+%%%% E.J.Partridge & J.P.Raaby
+%% Advanced Programming assignment 3
 
+-module(mr).
 -export([start/1, stop/1, job/5]).
 
 %%%% Interface
 
+% Starts a Mapreduce coordinator with N mapper nodes
 start(N) ->
     {Reducer, Mappers} = init(N),
     {ok, spawn(fun() -> coordinator_loop(Reducer, Mappers) end)}.
 
 
-stop(Pid) -> ....
+stop(Pid) ->
+    % send a synchronous message to the coordinator
+    rpc(Pid, stop).
+    % Pid ! die.
+    %TODO check this
 
-job(CPid, MapFun, RedFun, RedInit, Data) -> ....
+% Job function
+% Set the functions
+% Send the data
+job(CPid, MapFun, RedFun, RedInit, Data) ->
+    info(CPid,{mapper_setup,MapFun}),
+    io:format("Input: ~p ~n", [Data]),
+    info(CPid,{data,Data}),
+    % getting the result should be synchronous
+    Result = rpc(CPid,{reducer_setup,RedFun,RedInit,length(Data)}),
+    {ok,Result}.
 
 
 %%%% Internal implementation
+init(N) -> 
+    Reducer = spawn(fun() -> reducer_loop() end),
+    io:format("Started reducer ~p ~n", [Reducer]),
+    Mappers = for(1,N,fun() -> spawn(fun() -> mapper_loop(Reducer,unready) end) end),
+    io:format("Started ~p mappers ~n", [length(Mappers)]),
+    {Reducer,Mappers}.
 
 
-init(N) -> ....
-
+% "For" loop construction (from Programming Erlang, Joe Armstrong)
+for(N,N,F) -> [F()];
+for(I,N,F) -> [F()|for(I+1,N,F)].
 
 %% synchronous communication
-
+% This is Joe Armstrong's syntax!
 rpc(Pid, Request) ->
     Pid ! {self(), Request},
     receive
-	{Pid, Response} ->
-	    Response
+        {Pid, Response} ->
+            Response
     end.
 
 reply(From, Msg) ->
@@ -40,7 +65,6 @@ reply_ok(From, Msg) ->
 
 
 %% asynchronous communication
-
 info(Pid, Msg) ->
     Pid ! Msg.
 
@@ -58,12 +82,30 @@ setup_async(Pid, Fun) ->
 
 coordinator_loop(Reducer, Mappers) ->
     receive
-	{From, stop} ->
-	    io:format("~p stopping~n", [self()]),
-	    lists:foreach(fun stop_async/1, Mappers),
-	    stop_async(Reducer),
-	    reply_ok(From);
-	....
+        {From, stop} ->
+            io:format("~p stopping~n", [self()]),
+            lists:foreach(fun stop_async/1, Mappers),
+            stop_async(Reducer),
+            reply_ok(From);
+            
+        {mapper_setup,MapFun} ->
+            io:format("~p setting up mappers~n", [self()]),
+            lists:foreach(fun(M) -> setup_async(M,MapFun) end,Mappers),
+            coordinator_loop(Reducer,Mappers);
+            
+        {data,D} ->
+            io:format("~p starting ~n", [self()]),
+            send_data(Mappers,D),
+            coordinator_loop(Reducer,Mappers);
+            
+        {From,{reducer_setup,RedFun,RedInit,Size}} ->
+            io:format("~p setting up reducer~n", [self()]),
+            reply_ok(From,rpc(Reducer,{start_and_gather,RedFun,RedInit,Size})),
+            coordinator_loop(Reducer,Mappers);
+
+        Unknown ->
+            io:format("unknown message: ~p~n",[Unknown]), 
+            coordinator_loop(Reducer,Mappers)
     end.
 
 
@@ -79,30 +121,53 @@ send_loop(Mappers, [], Data) ->
 
 
 %%% Reducer
-
 reducer_loop() ->
     receive
-	stop -> 
-	    io:format("Reducer ~p stopping~n", [self()]),
-	    ok;
-        ....
+        stop -> 
+            io:format("Reducer ~p stopping~n", [self()]),
+            ok;
+        {From,{start_and_gather,Fun,I,N}} ->
+            io:format("Reducer ~p starting to wait~n", [self()]),
+            Result = gather_data_from_mappers(Fun,I,N),
+            reply(From,Result),
+            reducer_loop()
+            
+        % Unknown ->
+        %          io:format("reducer: unknown message: ~p~n",[Unknown]), 
+        %          reducer_loop()
     end.
 
+gather_data_from_mappers(_,Acc,0) ->
+    io:format("Reducer ~p received all data: ~p~n", [self(),Acc]),
+    Acc;
+    
 gather_data_from_mappers(Fun, Acc, Missing) ->
+    % io:format("Reducer ~p waiting~n", [self()]),
     receive
-	...
+        {data,D} ->
+            % io:format("Reducer ~p received data: ~p. ~p remaining~n", [self(),D,Missing]),
+            gather_data_from_mappers(Fun,Fun(D,Acc),Missing-1)
     end.
 
 
 %%% Mapper
-
-mapper_loop(Reducer, Fun) ->
+mapper_loop(Reducer,Fun) ->
     receive
-	stop -> 
-	    io:format("Mapper ~p stopping~n", [self()]),
-	    ok;
-	....
-	Unknown ->
-	    io:format("unknown message: ~p~n",[Unknown]), 
-	    mapper_loop(Reducer, Fun)
+        stop -> 
+            io:format("Mapper ~p stopping~n", [self()]),
+            ok;
+            
+        {setup, NewFun} -> % Sets the loop to have the given Function
+            % io:format("Mapper ~p starting ~n", [self()]),
+            mapper_loop(Reducer,NewFun);
+            
+        {data, D} -> % process and send to reducer
+            data_async(Reducer,Fun(D)),
+            io:format("Mapper - got data: ~p~n",[D]), 
+            
+            mapper_loop(Reducer,Fun);
+            
+        Unknown ->
+            io:format("Mapper - unknown message: ~p~n",[Unknown]), 
+            mapper_loop(Reducer, Fun)
     end.
---
+++ Assignment3/code/test_mr.erl
@@ -0,0 +1,43 @@
+-module(test_mr).
+-import(mr).
+-import(read_mxm).
+-export([test_jens/0,test_sum/0]).
+
+test_readmxm(File) ->
+    {Words,_} = read_mxm:from_file(File),
+    length(Words).
+
+test_jens() ->
+    {ok,MR} = mr:start(5),
+    mr:stop(MR).
+    
+test_sum() ->
+    {ok,MR} = mr:start(3),
+    {ok,Sum} = mr:job(MR,
+                        fun(X) -> X end,
+                        fun(X,Acc) -> X+Acc end,
+                        0,
+                        lists:seq(1,10)),
+    {ok, Fac} = mr:job(MR,
+                               fun(X) -> X end,
+                               fun(X,Acc) -> X*Acc end,
+                               1,
+                               lists:seq(1,10)),
+    mr:stop(MR),
+    {Sum,Fac}.
+    
+% test() ->
+%     % Read in the test file
+%     
+%     wc_dir(".").
+%     
+% wc_dir(Dir) ->
+%     "hello".
+%         %%%% setup the mapreduce here
+%         
+% generate_words(Pid,File) ->
+%     F = fun(Word) -> Pid ! {Word, 1} end.
+%     lib_misc:foreachWordInFile(File,F).
+%             
+% count_words(Key,Vals,A) ->
+%     [{length(Vals), Key}|A].
